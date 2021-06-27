01264a26fd9662809c491dd5b53b46f39fbf05aa
Yosada Dede <masyosad@gmail.com>
2021-06-27 20:13:07 +0700

    slicing splash_page

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,15 @@
-import 'package:cozy/pages/home_pages.dart';
+import 'package:cozy/pages/splash_page.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
-      theme: ThemeData(
-        // textTheme: GoogleF
-      ),
+      home: SplashPage()
     );
   }
 }
