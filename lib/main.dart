ffada021409be3a942ab351ad6a30dba40ac25f0
Yosada Dede <masyosad@gmail.com>
2021-06-27 08:24:11 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:cozy/pages/home_pages.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+      theme: ThemeData(
+        // textTheme: GoogleF
+      ),
+    );
+  }
+}
---
+++ lib/theme.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+Color purpleColor = Color(0xff5843BE);
+Color orangeColor = Color(0xffFF9376);
